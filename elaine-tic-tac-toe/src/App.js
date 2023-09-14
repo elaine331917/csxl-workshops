eeb661df91d5b3a4a5ea6d29bc65bd9f79d8e980
Elaine Dong <elaine13@email.unc.edu>
2023-09-14 00:14:16 -0400

    highlight winning squares

--- elaine-tic-tac-toe/src/App.js
+++ elaine-tic-tac-toe/src/App.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
 
-function Square({value, onSquareClick}) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>
+function Square({ value, onSquareClick, isWinningSquare }) {
+  const classNames = isWinningSquare ? "square winning" : "square";
+  return <button className={classNames} onClick={onSquareClick}>{value}</button>
 }
 
 function calculateWinner(squares) {
   const lines = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
-  for (let i=0; i < lines.length; i++) {
-    const [a,b,c] = lines[i];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        winningSquares: [a, b, c],
+      };
     }
   }
   return null;
@@ -35,34 +39,41 @@ function Board({ xIsNext, squares, onPlay }) {
     } else {
       nextSquares[i] = 'O';
     }
-    onPlay(nextSquares)
+    const winnerInfo = calculateWinner(nextSquares);
+    if (winnerInfo) {
+      onPlay(nextSquares, winnerInfo.winningSquares);
+    } else {
+      onPlay(nextSquares, []);
+    }
   }
 
-  const winner = calculateWinner(squares);
+  const winnerInfo = calculateWinner(squares);
   let status;
-  if (winner) {
-    status = "Winner: " + winner + "!";
+  if (winnerInfo) {
+    status = "Winner: " + winnerInfo.winner + "!";
+  } else if (squares.every((square) => square)) {
+    status = "It's a draw!";
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
-  
+
   return (
     <div>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(2)}/>
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(5)}/>
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} isWinningSquare={winnerInfo && winnerInfo.winningSquares.includes(8)}/>
       </div>
     </div>
   );
