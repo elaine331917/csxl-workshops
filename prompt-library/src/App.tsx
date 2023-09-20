ea1b0e0ea3d129658ead267dd7579381a5f322b3
Elaine Dong <elaine13@email.unc.edu>
2023-09-20 17:43:19 -0400

    add colors

--- prompt-library/src/App.tsx
+++ prompt-library/src/App.tsx
@@ -27,23 +27,23 @@ interface GridProps {
 function PromptCard({ prompt }: PromptCardProps) {
   return (
     <>
-      <Card className="inline-flex flex-col custom-card border-plum-700 bg-white">
-        <CardHeader className="flex flex-row custom-card-header w-72">
-          <Badge>{prompt.category}</Badge>
-          <Toggle>Favorite</Toggle>
+      <Card className="inline-flex flex-col custom-card border-2 rounded-xl border-plum-700 bg-white">
+        <CardHeader className="flex flex-row justify-between p-0 items-center custom-card-header w-72">
+          <Badge className="bg-plum-100 hover:bg-plum-200 text-plum-600 px-3 py-2 rounded-lg text-sm">{prompt.category}</Badge>
+          <Toggle className="text-plum-700">Favorite</Toggle>
         </CardHeader>
         <CardContent className="flex flex-col custom-card-content w-72">
           <CardTitle>{prompt.header}</CardTitle>
           <CardDescription className="flex text-left h-24 text-base text-black">{prompt.content}</CardDescription>
         </CardContent>
         <CardFooter className="flex flex-row p-0 custom-card-footer items-center justify-between w-72">
-          <div className="flex text-left text-sm text-slate-700">
+          <div className="flex text-left text-sm text-slate-500">
             Used {prompt.usage} times
           </div>
           <div className="flex flex-row items-center justify-between">
-            <Button>Up</Button>
-            <div className="px-2">{prompt.votes}</div>
-            <Button>Do</Button>
+            <Button className="bg-plum-700 hover:bg-plum-600">Up</Button>
+            <div className="px-2 text-plum-700">{prompt.votes}</div>
+            <Button className="bg-plum-700 hover:bg-plum-600">Down</Button>
           </div>
         </CardFooter>
       </Card>
@@ -60,7 +60,7 @@ function Grid({ prompts }: GridProps) {
   };
   
   return (
-    <div className="flex flex-wrap gap-1">
+    <div className="flex flex-wrap gap-4 justify-center">
       {prompts.map((prompt, index) => (
         <PromptCard
         key={`prompt-${index}`}
@@ -77,28 +77,28 @@ function App() {
     {
       header: "Prompt Header 1",
       content: "Prompt v1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
-      votes: 0,
+      votes: 101,
       category: "Education",
       usage: 234
     },
     {
       header: "Prompt Header 2",
       content: "Testing 2 Lorem ipsum dolor sit amet...",
-      votes: 0,
+      votes: 76,
       category: "Coding",
       usage: 123
     },
     {
       header: "Prompt Header 3",
       content: "Prompt v1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      votes: 0,
+      votes: 34,
       category: "Writing",
       usage: 234
     },
     {
       header: "Prompt Header 4",
       content: "Prompt v1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
-      votes: 0,
+      votes: 2,
       category: "Art",
       usage: 123
     },
@@ -120,27 +120,27 @@ function App() {
       header: "Prompt Header 7",
       content: "Prompt v1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
       votes: 0,
-      category: "Category 1",
+      category: "Coding",
       usage: 234
     },
     {
       header: "Prompt Header 8",
       content: "Prompt v1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
       votes: 0,
-      category: "Category 2",
+      category: "Homework",
       usage: 123
     },
     {
       header: "Prompt Header 9",
       content: "Prompt v1. Lorem ipsum dolor scing elit, sed doore magna aliqua...",
-      votes: 0,
-      category: "Category 2",
+      votes: -2,
+      category: "Education",
       usage: 123
     },
   ];
 
   return (
-    <div className="page flex flex-col items-center justify-center">
+    <div>
       <h1>Featured Prompts</h1>
       <br></br>
       <Grid prompts={prompts}></Grid>
