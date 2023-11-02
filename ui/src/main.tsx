40bcb66692c6792fb81d571ec9a9972d85007050
Elaine Dong <elaine13@email.unc.edu>
2023-11-02 11:07:21 -0400

    add basic router

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><App></App></div>,
+  },
+  {
+    path: '/prompts',
+    element: <div><App></App></div>,
+  },
+  {
+    path: '/categories',
+    element: <div>Coming soon!</div>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
